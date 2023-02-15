dcc78e9363182aad67db19792213493e61932084
diyanchuk <diynchuk@fmf.ru>
2023-02-16 01:01:25 +0400

    complete the CRUD cycle for user controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,16 +8,36 @@ class UserController {
       [name, surname]
     );
 
-    res.json(newPerson);
+    res.json(newPerson.rows[0]);
   }
 
-  async getUsers(req, res) {}
+  async getUsers(req, res) {
+    const users = await db.query("SELECT * FROM person");
+    res.json(users.rows);
+  }
 
-  async getCurrentUser(req, res) {}
+  async getCurrentUser(req, res) {
+    const id = req.params.id;
+    const user = await db.query("SELECT * FROM person where id = $1", [id]);
+    res.json(user.rows[0]);
+  }
 
-  async updateUser(req, res) {}
+  async updateUser(req, res) {
+    const { id, name, surname } = req.body;
+    const user = await db.query(
+      "UPDATE person set name = $1, surname = $2 where id = $3 RETURNING *",
+      [name, surname, id]
+    );
 
-  async deleteUser(req, res) {}
+    res.json(user.rows[0]);
+  }
+
+  async deleteUser(req, res) {
+    const id = req.params.id;
+    const user = await db.query("DELETE FROM person where id = $1", [id]);
+
+    res.json(user.rows[0]);
+  }
 }
 
 module.exports = new UserController();
