3324a5e361ad6668d594c24a52da7ce9b18e1f09
diyanchuk <diynchuk@fmf.ru>
2023-02-16 01:15:51 +0400

    codestyle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 
-const userRouter = require("./routes/user.routes");
+const userRouter = require('./routes/user.routes');
 
 const PORT = process.env.PORT || 9000;
 
@@ -8,12 +8,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api", userRouter);
+app.use('/api', userRouter);
 
-app.get("/", (req, res) => {
-  res.send("Working...");
+app.get('/', (req, res) => {
+  res.send('Working...');
 });
 
 app.listen(PORT, () => {
-  console.log("Server started");
+  console.log('Server started');
 });
