c77b8675ad9062bb87959bcc0b9c091277cbd6ee
diyanchuk <diynchuk@fmf.ru>
2023-02-17 23:53:04 +0400

    create users module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { CreateUserDto } from './dto/create-user.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+}
